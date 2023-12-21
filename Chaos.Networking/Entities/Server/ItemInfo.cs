db204b730b6bffb8e6ceb7e4eafb4bfac5323a1e
FallenDev <paul.buck@TheBuckNetwork.com>
2023-12-21 14:23:14 -0500

    Set Group for MenuArgs & Defaults

--- Chaos.Networking/Entities/Server/ItemInfo.cs
+++ Chaos.Networking/Entities/Server/ItemInfo.cs
@@ -10,37 +10,50 @@ public sealed record ItemInfo
     /// <summary>
     ///     The color of the item
     /// </summary>
-    public required DisplayColor Color { get; init; }
+    public required DisplayColor Color { get; init; } = DisplayColor.Default;
+
     /// <summary>
     ///     The cost of the item
     /// </summary>
-    public required int? Cost { get; init; }
+    public required int? Cost { get; init; } = 0;
+
     /// <summary>
     ///     The count of the item
     /// </summary>
-    public required uint? Count { get; init; }
+    public required uint? Count { get; init; } = 0;
+
     /// <summary>
     ///     The current durability of the item
     /// </summary>
-    public required int CurrentDurability { get; init; }
+    public required int CurrentDurability { get; init; } = 0;
+
     /// <summary>
     ///     The maximum durability of the item
     /// </summary>
-    public required int MaxDurability { get; init; }
+    public required int MaxDurability { get; init; } = 0;
+
     /// <summary>
     ///     The name of the item
     /// </summary>
-    public required string Name { get; init; } = null!;
+    public required string Name { get; init; } = "";
+
+    /// <summary>
+    ///     The group of the item
+    /// </summary>
+    public required string Group { get; init; } = "";
+
     /// <summary>
     ///     The slot of the item
     /// </summary>
-    public required byte Slot { get; init; }
+    public required byte Slot { get; init; } = 0;
+
     /// <summary>
     ///     The sprite of the item
     /// </summary>
-    public required ushort Sprite { get; init; }
+    public required ushort Sprite { get; init; } = 0;
+
     /// <summary>
     ///     Whether or not the item is stackable
     /// </summary>
-    public required bool Stackable { get; init; }
+    public required bool Stackable { get; init; } = false;
 }
